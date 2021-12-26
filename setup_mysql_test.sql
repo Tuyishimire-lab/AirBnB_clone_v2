3623d075e1091485e45a1685578073e5db5e9991
Aficionado01 <emmanuelugochukwu530@gmail.com>
2021-12-25 17:27:13 -0800

    setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,9 @@
--- prepares a MySQL test server for the AirBnb clone
--- creates a database
+-- Create a database
+-- Create hbnb_test_db if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- create a user and grant privileges
-GRANT ALL ON hbnb_test_db.*
-TO 'hbnb_test'@'localhost'
-IDENTIFIED BY 'hbnb_test_pwd';
--- grant SELECT privileges
-GRANT SELECT ON performance_schema.*
-TO 'hbnb_test'@'localhost';
-
+-- Create user:hbnb_test with password 'hbnb_test_pwd'
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+-- Privilege to hbnb_test on database: hbnb_test_db
+GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+-- Privilege to hbnb_test on database: performance_schema
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
